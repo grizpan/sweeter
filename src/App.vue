78347e730780d5155ea288808fa2b638366602a1
mikhailplakhotya <mikhail.ukraine@gmail.com>
2017-01-16 21:51:44 +0200

    Initial commit sweeter-vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+    <img class="app__img" src="./assets/logo.png">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="scss">
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  &__img {
+    max-width: 100%;
+  }
+}
+</style>
